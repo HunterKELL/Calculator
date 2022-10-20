a8cc93d13930971901c5fffefd3ea886a24021ee
Cristian <christianchapa@yahoo.com>
2022-10-20 00:06:20 -0400

    Added basic math operations

---
+++ app.js
@@ -0,0 +1,27 @@
+addNum = (a, b) => {
+    if (result = a + b) {
+        return result;
+    }
+}
+console.log(addNum(2, 4));
+
+minusNum = (a, b) => {
+    if (result = a - b) {
+        return result;
+    }
+}
+console.log(minusNum(4, 2));
+
+multiplyNum = (a, b) => {
+    if (result = a * b) {
+        return result;
+    }
+}
+console.log(multiplyNum(2, 4));
+
+divideNum = (a, b) => {
+    if (result = a / b) {
+        return result;
+    }
+}
+console.log(divideNum(4, 2));
