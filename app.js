78f5273054bb67dfa8966d00a51ded2e8feb08ea
Cristian <christianchapa@yahoo.com>
2022-11-03 23:28:49 -0400

    Fixed html class causing decimal button to not work as intended

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const display = document.querySelector('.display-screen');
 const firstDisplay = document.querySelector('.bottom-screen');
 const secondDisplay = document.querySelector('.top-screen');
 const numberBtn = document.querySelectorAll('.number');
@@ -6,25 +5,13 @@ const operatorBtn = document.querySelectorAll('.operator');
 const clear = document.querySelector('.clear-btn');
 const equal = document.querySelector('.equal-btn');
 const decimalBtn = document.querySelector('.decimal-btn');
-const percent = document.querySelector('.percent-btn');
 const squareRoot = document.querySelector('.square-root');
-//const numButtons = document.getElementById("number");
-//const calcButtons = document.querySelectorAll('.calc-button');
-console.log(numberBtn);
-console.log(operatorBtn);
-console.log(clear);
-console.log(equal);
-console.log(firstDisplay);
-console.log(secondDisplay);
-console.log(decimalBtn)
-
 
 let currentNum = ''
 let previousNum = ''
 let operator = ''
 //let decimal = ''
 
-//console.log(logNumber())
 // Event listener for all number buttons. Found it easier to keep the logic of displaying numbers all within instead of within another function.
 numberBtn.forEach(number => {
     number.addEventListener('click', (e) => {
@@ -54,11 +41,10 @@ const logOperator = (oper) => {
 };
 
 const logDecimal = (dot) => {
-    console.log(dot)
-    if (!firstDisplay.textContent.indexOf(dot)){
-        firstDisplay.textContent = 0 + dot;
-    } else if (dot.length < 1){
-        firstDisplay.textContent = dot;
+    //console.log(dot)
+    if (!currentNum.includes('.')){
+        currentNum += '.';
+        firstDisplay.textContent = currentNum;
     }
 };
 decimalBtn.addEventListener('click', (e) => {
@@ -75,8 +61,8 @@ clear.addEventListener('click', () => {
     previousNum = '';
     currentNum = '';
     operator = '';
-    firstDisplay.textContent = '';
-    secondDisplay.textContent = '';
+    firstDisplay.textContent = '0';
+    secondDisplay.textContent = '0';
 });
 
 calculate = () => {
@@ -108,7 +94,6 @@ equal.addEventListener('click', () => {
             firstDisplay.textContent = previousNum;
         }
     }
-   
 })
 
 const findSqRt = () => {
